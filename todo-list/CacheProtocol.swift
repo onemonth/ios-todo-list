1e119e6397f99e542b8a89a6d887c2866d07c008
Alfie Hanssen <alfiehanssen@gmail.com>
2015-08-29 14:35:11 -0400

    created cache protocol

---
+++ todo-list/CacheProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  CacheProtocol.swift
+//  todo-list
+//
+//  Created by Alfred Hanssen on 8/29/15.
+//  Copyright © 2015 One Month. All rights reserved.
+//
+
+import Foundation
+
+protocol CacheProtocol {
+    func loadObjectForKey(key: NSString)
+    func saveObject(object: NSObject, forKey: NSString)
+    func removeObjectForKey(key: NSString)
+}
