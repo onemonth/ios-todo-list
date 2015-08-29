e6a2027f2f89eddbe5afcea50af6aa6369359f27
Alfie Hanssen <alfiehanssen@gmail.com>
2015-08-29 13:47:35 -0400

    added UITextView to AddItemViewController and connected IBOutlet

---
+++ todo-list/AddItemViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddItemViewController.swift
+//  todo-list
+//
+//  Created by Alfred Hanssen on 8/29/15.
+//  Copyright © 2015 One Month. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    static let Title = "Add Item"
+    
+    @IBOutlet weak var textView: UITextView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.title = AddItemViewController.Title        
+    }
+
+}
